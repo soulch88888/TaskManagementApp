15150390297aeb1a8f2fed422485e5ffadb18132
soulch88888 <s.c.youg@gmail.com>
2024-04-23 09:39:16 +0300

    removed comments

--- signup_action.php
+++ signup_action.php
@@ -6,17 +6,8 @@
     $email=$_POST['email'];
     $pass=$_POST['pass'];
 
-
-    /* 
-    This method gives the query to insert rows in the table
-    echo("The att are: $username, $email, $pass");
-    $query="INSERT INTO `user`(`name`, `email`, `pass`) VALUES ('$username','$email','$pass')";
-    echo($query);
-    */
-
-    //This method inserts the row automatically in the table
     $query="INSERT INTO `user`(`username`,`first_name`,`last_name`, `email`, `password`) VALUES ('$username','$fname','$lname','$email','$pass')";
     mysqli_query($con,$query);
     
-    header("Location: login.php"); //for redirection to the login page after inserting the row
+    header("Location: login.php"); 
 ?>
